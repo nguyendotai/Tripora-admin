2c7f0853aedcefbb0f97aff3624b5ef7ff73b23f
nguyendotai <nguyendotai11@gmail.com>
2026-08-11 20:50:12 +0700

    Reset project cho roadmap V1-V9 moi (Tripora)

--- src/features/destination/index.ts
+++
@@ -1,9 +0,0 @@
-export {
-  useListDestinationsQuery,
-  useCreateDestinationMutation,
-  useUpdateDestinationMutation,
-  useDeleteDestinationMutation,
-} from './api/destination.api';
-export { destinationFormSchema } from './schemas/destination.schema';
-export type { DestinationFormValues } from './schemas/destination.schema';
-export type { Destination, PaginatedResult, PaginationMeta } from './types/destination.types';
